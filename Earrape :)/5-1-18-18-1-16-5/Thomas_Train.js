556311ca1b7b823748b6928c9fd77dd6c04308ad
someonenumber011 <62475963+someonenumber011@users.noreply.github.com>
2020-03-24 21:07:14 -0500

    Update Thomas_Train.js

--- Earrape :)/5-1-18-18-1-16-5/Thomas_Train.js
+++ Earrape :)/5-1-18-18-1-16-5/Thomas_Train.js
@@ -1,3 +1,16 @@
-var vid = document.getElementById("/audio/Thomas The Tank Engine.mp3");
+var vidNormal = document.getElementById("/audio/Thomas The Tank Engine.mp3");
 
-vid.volume = 0.2;
+var thomasNormal = new Howl({
+        src: ["audio/Thomas The Tank Engine.mp3"],
+        loop: true,
+});
+  
+var thomasBass = new Howl({
+        src: ["audio/Thomas_The_Tank_Engine_Bass_boosted.mp3"],
+        loop: true,
+});
+
+function run() {
+	thomasNormal.play();
+        thomasBass.play()
+}
